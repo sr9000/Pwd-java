6c8657d518a662fc50731f378721ae7815d553b0
Rogonov Stepan <rogonovstepan@gmail.com>
2017-12-26 22:25:00 +0300

    Fix first screen

--- src/Form1.java
+++ src/Form1.java
@@ -1,27 +1,21 @@
 import java.awt.CardLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JProgressBar;
-import javax.swing.JRadioButton;
+import javax.swing.JScrollBar;
 import javax.swing.JSpinner;
-import javax.swing.JTree;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class Form1 {
 
-  private JPanel panel1;
-  private JPanel Card1;
+  private JPanel panelMain;
+  private JPanel cardAlphabet;
   private JPanel Card2;
   private JButton nextButton;
   private JButton prevButton;
-  private JList list1;
-  private JButton editButton;
-  private JButton addButton;
-  private JButton removeButton;
   private JSpinner spinner1;
   private JButton nextButton2;
   private JPanel Card3;
@@ -33,37 +27,36 @@ public class Form1 {
   private JProgressBar progressBar1;
   private JButton removeButton1;
   private JList list2;
+  private JPanel cardViewChars;
+  private JButton addButton;
+  private JButton editButton;
+  private JButton removeButton;
+  private JButton backButton1;
+  private JTable table1;
+  private JPanel cardAddChars;
+  private JPanel cardEditChars;
+  private JButton loadTableButton;
+  private JButton saveTableButton;
+  private JTextField a0TextField;
 
   public Form1() {
-    nextButton.addActionListener(
-        e -> {
-          if (panel1.getLayout() instanceof CardLayout) {
-            ((CardLayout) panel1.getLayout()).next(panel1);
-          }
-        });
     prevButton.addActionListener(
         e -> {
-          if (panel1.getLayout() instanceof CardLayout) {
-            ((CardLayout) panel1.getLayout()).previous(panel1);
+          if (panelMain.getLayout() instanceof CardLayout) {
+            ((CardLayout) panelMain.getLayout()).previous(panelMain);
           }
         });
     nextButton2.addActionListener(
         e -> {
-          if (panel1.getLayout() instanceof CardLayout) {
-            ((CardLayout) panel1.getLayout()).next(panel1);
-          }
-        });
-    backButton.addActionListener(
-        e -> {
-          if (panel1.getLayout() instanceof CardLayout) {
-            ((CardLayout) panel1.getLayout()).previous(panel1);
+          if (panelMain.getLayout() instanceof CardLayout) {
+            ((CardLayout) panelMain.getLayout()).next(panelMain);
           }
         });
   }
 
   public static void main(String[] args) {
     JFrame frame = new JFrame("Form1");
-    frame.setContentPane(new Form1().panel1);
+    frame.setContentPane(new Form1().panelMain);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
